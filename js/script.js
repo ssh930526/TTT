dc54af7e16aeafcdfeb64d74192985c980384587
Sihui Shen <sihui@Sihuis-MBP.fios-router.home>
2021-03-10 01:15:58 -0500

    feat: adds working logic

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ const $gameboardEl = $('#gameboard');
 /*----- event listeners -----*/
 
 $buttonEl.click(init); // click to reset game
-
+$gameboardEl.on('click', '.square', handleClick);
 
 
 /*----- function -----*/
@@ -43,10 +43,41 @@ function init() {
     winner = false;
     turn = 1;
     gameboard = new Array(9).fill(null);
+    render();
 
 };
 
 
+function getWinner() {
+    for(let i = 0; i < COMBOS.length; i++) {
+        if(Math.abs(gameboard[COMBOS[i][0]] +
+                    gameboard[COMBOS[i][1]] +
+                    gameboard[COMBOS[i][2]]) === 3)
+                    return gameboard[COMBOS[i][0]]
+    }
+    if(gameboard.includes(null)) return false;
+    return 'T';
 
+}
 
+function handleClick(evt) {
+    const position = evt.target.dataset.index;
+    if(gameboard[position] || winner) return; // exit function immediately!
+    gameboard[position] = turn;
+    turn = turn * -1;// turn *= -1;
+    winner = getWinner();
+    render();
+}
 
+function render () {
+   gameboard.forEach(function(value, index) {
+       $squareEls.eq(index).text(PLAYERS[value])
+   });
+   if(!winner){
+   $messageEl.text(`${PLAYERS[turn]}'s Turn `)
+   } else if (winner === 'T') {
+       $messageEl.text('Tie Game');
+   } else {
+       $messageEl.text(`${PLAYERS[winner]} Wins!`);
+   }
+}
